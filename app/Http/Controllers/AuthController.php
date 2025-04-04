de4b07870f45a2411aeb5143fca2979a3703e401
Hector Martin Bazalar Contreras <39540671+MBazalar21@users.noreply.github.com>
2025-04-04 18:24:55 -0500

    Req-1: Endpoint Register User
    Api en Ruta Publica

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json(['message' => 'Usuario registrado exitosamente'], 201);
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+
+// Rutas públicas
+Route::post('/register', [AuthController::class, 'register']);
